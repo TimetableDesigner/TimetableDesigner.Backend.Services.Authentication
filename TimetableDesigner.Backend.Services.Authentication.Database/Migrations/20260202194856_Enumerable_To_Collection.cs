930f73d83d0b32b7d51d0d03a7ea5bb5bd5c8687
Mateusz Skoczek <mateusz5432160@gmail.com>
2026-02-03 00:05:40 +0100

    migration added

---
+++ TimetableDesigner.Backend.Services.Authentication.Database/Migrations/20260202194856_Enumerable_To_Collection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TimetableDesigner.Backend.Services.Authentication.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Enumerable_To_Collection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
